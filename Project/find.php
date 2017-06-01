615ab91039cfb7d67f02eae34fd86237b7bcb888
Joshua Melander <joshua.melander@hotmail.com>
2017-06-01 00:52:13 -0500

    Test 8 - Success
    -					$sql= "SELECT product
    +					$sql= "SELECT id, product, cost

--- Project/find.php
+++ Project/find.php
@@ -27,7 +27,7 @@
 
 					$product = (isset($_POST['product'])    ? $_POST['product']   : '');
 
-					$sql= "SELECT product
+					$sql= "SELECT id, product, cost
 					FROM product_t
 					WHERE product LIKE '$product' LIMIT 1";
 					$result = mysqli_query($conn, $sql);
